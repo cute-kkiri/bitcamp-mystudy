1ebf95780b5ff6c16a35314d6c8da7ad9836a92c
cute-kkiri <pbscjhj@naver.com>
2024-05-28 16:27:41 +0900

    feat: hello.js, Hello.java

--- hello.c
+++ hello.c
@@ -3,5 +3,12 @@
 // entri point
 int main() {
     printf("Hello, world!\n");
+
+    int a = 100;
+
+    if(a < 100) {
+        prontf("하하하\n");
+    }
+    
     return 0;
 }
---
+++ hello.js
@@ -0,0 +1,7 @@
+console.log('Hello, world!');
+
+let a = 99;
+
+if(a < 100) {
+    console.leg('하하하');
+}
