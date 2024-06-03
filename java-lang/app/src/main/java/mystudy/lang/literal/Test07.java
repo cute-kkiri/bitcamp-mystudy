615f0302129625328bc59fcc2ed0a40a06c09f2c
cute-kkiri <pbscjhj@naver.com>
2024-06-03 19:18:09 +0900

    mystudy/literal ~13

---
+++ java-lang/app/src/main/java/mystudy/lang/literal/Test07.java
@@ -0,0 +1,20 @@
+package mystudy.lang.literal;
+
+// # 부동소수점 리터럴
+// - 실수 값을 부동소수점 방식으로 저장하려면 1과 0으로 변환해야 한다.
+// - 전기전가기술자협회(IEEE)에서 개발한 IEEE 754 명세에 따라 2진수로 변환한다.
+//
+public class Test07 {
+    public static void main(String[] args) {
+        // ## 10진수 표기법
+        System.out.println(3.14);
+        // - e 기호를 사용하면 소수점의 위치를 조정할 수 있다.
+        // - 소수점의 위치를 움직인다고 해서 "부동소수점(floating point)"이라 부른다.
+        // - 표기법
+        // 숫자e지수 또는 숫자E지수
+        System.out.println(0.0314e2); // 0.0314 + 10의2승 = 3.14
+        System.out.println(0.314e1); // 0.314 + 10의1승 = 3.14
+        System.out.println(31.4e-1); // 31.4 + 10의-1승 = 3.14
+        System.out.println(314e-2); // 314 + 10의-2승 = 3.14
+    }
+}
---
+++ java-lang/app/src/main/java/mystudy/lang/literal/Test13.java
@@ -0,0 +1,8 @@
+package mystudy.lang.literal;
+
+// # 문자의 리터럴 - 문자 집합(character set)
+
+public class Test13 {
+    public static void main(String[] args) {
+    }
+}
